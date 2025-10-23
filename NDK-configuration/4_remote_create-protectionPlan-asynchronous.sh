c6586f398cdb8cd2fb462516ff5822201f52ed3c
Eric De Witte <eric@vedw.eu>
2025-10-23 22:50:49 +0200

    add scheduler

--- NDK-configuration/4_remote_create-protectionPlan-asynchronous.sh
+++ NDK-configuration/4_remote_create-protectionPlan-asynchronous.sh
@@ -74,6 +74,21 @@ select REPLICATIONTARGET in $REPLICATIONTARGETS; do
     break
 done
 
+#Select jobscheduler in namespace
+JOBSCHEDULERS=$(kubectl get JobScheduler -n $SOURCENAMESPACE --no-headers=true |awk '{print $1}' |sort -u)
+#check if empty
+if [ -z "$JOBSCHEDULERS" ]; then
+    echo "No JobSchedulers found in namespace $SOURCENAMESPACE. Please create a JobScheduler first."
+    exit 1
+fi
+echo
+echo "Select jobscheduler or CTRL-C to quit"
+select JOBSCHEDULER in $JOBSCHEDULERS; do 
+    echo "you selected jobscheduler : ${JOBSCHEDULER}"
+    echo 
+    break
+done
+
 #select retention count - min is 1 max is 15
 echo
 echo "Enter snapshot retention count (number of snapshots to keep - between 1 and 15):"
@@ -96,6 +111,7 @@ metadata:
  namespace: $SOURCENAMESPACE
 spec: 
  protectionType: async
+ scheduleName: $JOBSCHEDULER 
  replicationConfigs:
    - replicationTargetName: $REPLICATIONTARGET
  retentionPolicy: 
