7ee3cc640e1937594bff68ac965a036856e0a2d5
Eric De Witte <eric@vedw.eu>
2026-02-22 10:34:08 +0100

    get ndkcli

---
+++ 0-get-ndkcli.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+#------------------------------------------------------------------------------
+
+# Copyright 2024 Nutanix, Inc
+#
+# Licensed under the MIT License;
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
+# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#------------------------------------------------------------------------------
+
+
+# Prompt the user for the download link
+echo 'open browser to site : https://portal.nutanix.com/page/downloads?product=ndk and find "CLI for Nutanix Data Services for Kubernetes (linux-amd64)" '
+read -p "Enter download link: " url < /dev/tty
+
+# Check if URL is empty
+if [ -z "$url" ]; then
+    echo "No URL provided. Exiting."
+    exit 1
+fi
+
+# Curl the file and pipe straight to tar to extract to /usr/local/bin
+curl -fsSL "$url" | sudo tar xz -C /usr/local/bin -- ndkcli
+
+# Success message
+echo "NDK CLI installed successfully!"
+echo "checking version"
+ndkcli version
